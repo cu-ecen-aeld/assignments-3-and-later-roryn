f4a29f5d6425668fded6fade9d2e4796a468c631
Rory <rorynicolls@gmail.com>
2023-07-26 21:33:19 -0700

    Fixed unit test and added finder script

---
+++ finder-app/finder.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dir=$1
+str=$2
+
+if [[ -z "$str" ]] || [[ -z "$dir" ]]
+then
+	echo "Missing parameter"
+	exit 1
+fi
+
+if [[ ! -d "$dir" ]]
+then
+	echo "Directory does not exist"
+	exit 1
+fi
+
+var1=$(grep -r -o "$str*" "$dir" | wc -l)
+var2=$(grep -r -l "$str*" "$dir" | wc -l)
+
+
+echo "The number of files are ${var2} and the number of matching lines are ${var1}"
+

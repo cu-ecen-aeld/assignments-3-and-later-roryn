bf9c24d82e203ca76f8e3cf2344927e9ef5333b9
roryn <rorynicolls@gmail.com>
2023-12-10 21:11:32 -0800

    First commit of assignment 3b

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -33,21 +33,28 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
-
-    # TODO: Add your kernel build steps here
+    cd "$OUTDIR/linux-stable"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
-echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
-then
+then                
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+mkdir -p rootfs
+cd rootfs
+mkdir -p bin dev etc hoe lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -60,21 +67,60 @@ else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd "$OUTDIR"
+cp -r bin ${OUTDIR}/rootfs
+cp -r sbin ${OUTDIR}/rootfs
+cp -r usr ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+find /home/rory/ -type f -name "lib/ld-linux-aarch64.so.1" -exec echo {} \;
+find /home/rory/ -type f -name "libm.so.6" -exec echo {} \;
+find /home/rory/ -type f -name "libresolv.so.2" -exec echo {} \;
+find /home/rory/ -type f -name "libc.so.6" -exec echo {} \;
+
+find /home/rory/ARM_ToolChain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/ -type f -name "ld-linux-aarch64.so.1" -exec echo {} \;
+find /home/rory/ARM_ToolChain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/ -type f -name "libm.so.6" -exec echo {} \;
+find /home/rory/ARM_ToolChain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/ -type f -name "libresolv.so.2" -exec echo {} \;
+find /home/rory/ARM_ToolChain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/ -type f -name "libc.so.6" -exec echo {} \;
+
+find /home/rory/ -type f -name "ld-linux-aarch64.so.1" -exec cp {} ${OUTDIR}/rootfs/lib/ \;
+find /home/rory/ -type f -name "libm.so.6" -exec cp {} ${OUTDIR}/rootfs/lib64/ \;
+find /home/rory/ -type f -name "libresolv.so.2" -exec cp {} ${OUTDIR}/rootfs/lib64/ \;
+find /home/rory/ -type f -name "libc.so.6" -exec cp {} ${OUTDIR}/rootfs/lib64/ \;
 
 # TODO: Make device nodes
+cd "$OUTDIR/rootfs"
+sudo mknod -m 0666 dev/null c 1 3
+sudo mknod -m 0666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ~/Assignments/assignment-1-roryn/finder-app
+make CROSS_COMPILE=aarch64-none-linux-gnu- clean
+make CROSS_COMPILE=aarch64-none-linux-gnu- build
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 
+mkdir ${OUTDIR}/rootfs/home
+cp writer.c writer.o finder.sh finder-test.sh writer.sh writer ${OUTDIR}/rootfs/home
+mkdir ${OUTDIR}/rootfs/home/conf
+cp -r conf/username.txt ${OUTDIR}/rootfs/home/conf
+cp -r conf/assignment.txt ${OUTDIR}/rootfs/home/conf
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
+
 # TODO: Chown the root directory
+sudo chmod -R 777 ${OUTDIR}/
 
 # TODO: Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio

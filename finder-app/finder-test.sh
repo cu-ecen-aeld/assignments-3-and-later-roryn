a4d2bfebbf6c932bc7d768c9d1b1067c11521a4c
roryn <rorynicolls@gmail.com>
2024-02-10 17:10:54 -0800

    Adding script path to utility calls

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -9,7 +9,8 @@ NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 RESULTDIR=/tmp
-SCRIPTDIR=/etc/finder-app
+CONFDIR=/etc/finder-app
+SCRIPTDIR=$(dirname $0)
 username=$(cat ${SCRIPTDIR}/conf/username.txt)
 
 if [ $# -lt 3 ]
@@ -54,15 +55,15 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	${SCRIPTDIR}/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(${SCRIPTDIR}/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
-./writer "$RESULTDIR/assignment-4-result.txt" "$OUTPUTSTRING"
+${SCRIPTDIR}/writer "$RESULTDIR/assignment-4-result.txt" "$OUTPUTSTRING"
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
